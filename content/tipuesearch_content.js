d74ea053334b156f53824774a1ed86c1f008e3f0
40823236 <40823236@gm.nfu.edu.tw>
2019-11-14 14:58:50 +0800

    add graphics files

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019/content/index.html \xa0 \n', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019/content/index.html \xa0 \n https://gist.github.com/40823236/4914fdc29335ed5def3461f1db3001ef \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}]};
