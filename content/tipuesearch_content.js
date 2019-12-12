3d96f4af9fb41b74191af30e7df38f766ef06627
40823236 <40823236@gm.nfu.edu.tw>
2019-12-12 13:38:45 +0800

    add dart-ex

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019/content/index.html \xa0 \n https://gist.github.com/40823236/4914fdc29335ed5def3461f1db3001ef \n \n \n USB', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019/content/index.html \xa0 \n https://gist.github.com/40823236/4914fdc29335ed5def3461f1db3001ef \n \n \n https://gist.github.com/40823236/5ca7f3fbb94069667e3dd01914987542 \n USB', 'tags': '', 'url': 'About.html'}]};
